ddf4001598c0a7d0c5beaf0d5934d760b0f6601b
Narvaez2001 <ncnrvz2001@gmail.com>
2022-12-16 20:40:30 -0500

    update

--- Pokemons/js/index.js
+++ Pokemons/js/index.js
@@ -4,7 +4,7 @@
 const POKEMONS_CRUD_DATA = 'Pokemons';
 const pokemons = JSON.parse(localStorage.getItem(POKEMONS_CRUD_DATA)) ?? [];
 
-const createPokemon = (e) => {
+const createPokemon = () => {
     e.preventDefault();
     const documentFormPokemon = document.forms['formPokemon'];
     const name = documentFormPokemon.name.value;
@@ -56,12 +56,37 @@ const readPokemons = () => {
 };
 
 const readPokemon = (index) => {
-    const pokemon = pokemons.find((_, i) => {
-        return i === index;
-    });
-    console.log(pokemon);
+    // const pokemon = pokemons.find((_, i) => {
+    //     return i === index;
+    const documentFormPokemon = document.forms['formPokemon'];
+    const pokemon = pokemons.slice(index, index + 1)[0];
+    const { name, type, hp, attack, special, imgurl } = pokemon;
+    documentFormPokemon.index.value = index;
+    documentFormPokemon.name.value = name;
+    documentFormPokemon.type.value = type;
+    documentFormPokemon.hp.value = hp;
+    documentFormPokemon.attack.value = attack;
+    documentFormPokemon.special.value = special;
+    documentFormPokemon.imgurl.value = imgurl;
+    document.getElementById('button').innerText = 'Editar';
+
+
 };
 
+const updatePokemon = (index) => {
+    const documentFormPokemon = document.forms['formPokemon'];
+    const name = documentFormPokemon.name.value;
+    const type = documentFormPokemon.type.value;
+    const hp = documentFormPokemon.hp.value;
+    const attack = documentFormPokemon.attack.value;
+    const special = documentFormPokemon.special.value;
+    const imgurl = documentFormPokemon.imgurl.value;
+    pokemons.splice(index, 1, { name, type, hp, attack, special, imgurl });
+    localStorage.setItem(POKEMONS_CRUD_DATA, JSON.stringify(pokemons));
+    documentFormPokemon.reset();
+    readPokemons();
+    document.getElementById('button').innerText = 'Crear';
+}
 
 const deletePokemon = (index) => {
 
@@ -91,20 +116,26 @@ const deletePokemon = (index) => {
                 'Cancelado',
                 'Te salvaste pelon:',
                 'error'
-            )
+            );
         }
-    })
+    });
 
 };
-
-
-
-
 
 const documentReady = () => {
     const formPokemon = document.getElementById('formPokemon');
+    const submitPokemon = (e) => {
+        e.preventDefault();
+        const index = document.getElementById('index').value;
+        // (index === '' && console.log('create'));
+        // (index === '' || console.log('update'));
+        if (index === '') {
+            createPokemon();
+        } else {
+            updatePokemon(index);
+        }
+    };
     readPokemons();
-    formPokemon.addEventListener('submit', createPokemon);
+    formPokemon.addEventListener('submit', submitPokemon);
 };
-
 document.addEventListener('DOMContentLoaded', documentReady);
